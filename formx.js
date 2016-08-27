2bb615cbec5d1bd96c161db258836283d2c5abd2
imsole <macore@163.com>
2016-08-27 16:12:10 +0800

    Update formx.js

--- formx.js
+++ formx.js
@@ -1,3 +1,10 @@
+/**
+ * @name form validate widget
+ * @auth sole
+ * @time 2015年8月3日
+ * @verson 1.0
+ * @use $('#form').formx();
+*/
 ;(function($){
 	$.fn.getForm = function(type){
 		var _this = $(this),
@@ -24,13 +31,6 @@
 		return _res;
 	};
 })(jQuery);
-/**
- * @name form validate widget
- * @auth sole
- * @time 2015年8月3日
- * @verson 1.0
- * @use $('#form').formx();
-*/
 
 ;(function($){
 
@@ -85,145 +85,97 @@
 
 				(oRequired.length==arr.length && !iErr) ? oBtnSave.removeAttr('disabled') : oBtnSave.attr('disabled', 'disabled');
 			}
+			function v_empty(_this){
+				var _val = $.trim( _this.val() ),
+					_parent = _this.parents(opt.errElem);
+				_parent[ (!_val ? 'add' : 'remove') + 'Class' ](opt.errClass);
+				_parent[ (!_val ? 'remove' : 'add') + 'Class' ](opt.okClass);
+				return !_val ? false : true;
+			}
+
+			function v_pattern(_this){
+				var _val = $.trim( _this.val() ),
+					_parent = _this.parents(opt.errElem),
+					_name = _this.attr('name'),
+					_re = reTxt[ _name ];
+				var reg = new RegExp(_re);
+				_parent[ (!reg.test(_val) ? 'add' : 'remove') + 'Class' ](opt.errClass);
+				_parent[ (!reg.test(_val) ? 'remove' : 'add') + 'Class' ](opt.okClass);
+				if( reg.test(_val) ){
+					opt.regexped( _this );
+				}
+			}
+
+			function v_url(_this){
+				var _val = $.trim( _this.val() ),
+					_parent = _this.parents(opt.errElem),
+					_url = _this.data('url'),
+					_name = _this.attr('name');
+				var data = {};
+				data[_name] = _val;
+				$.ajax({
+					async: false,
+					type: 'post',
+					dataType: 'json',
+					data: data,
+					url:_url,
+					success:function(res){
+						_parent[ (!res.code ? 'add' : 'remove') + 'Class' ](opt.errClass);
+						_parent[ (!reg.code ? 'remove' : 'add') + 'Class' ](opt.okClass);
+						if( res.code == 1 ){
+							_parent.removeClass(opt.errClass).addClass(opt.okClass);
+						}else{
+							_parent.addClass(opt.errClass).removeClass(opt.okClass);
+						}
+						opt.urled(_this, res);
+					}
+				});
+			}
+
+			function v_group(_this){
+				var _group = _this.data('group');
+				var oGroup = oForm.find('input[data-group="'+_group+'"]');
+				var aGroup = oGroup.parents(opt.errElem);
+				var arr = [];
+				oGroup.each(function(i, ele){
+					if( !$.trim($(ele).val()) ){ arr.push(i); }
+				});
+
+				if( !arr.length ){
+					if( oGroup.eq(0).val() === oGroup.eq(1).val() ){
+						aGroup.removeClass(opt.errClass).addClass(opt.okClass);
+					}else{
+						aGroup.addClass(opt.errClass).removeClass(opt.okClass);
+					}
+				}
+			}
+
+			function v_event(_this, eventName){
+					_parent = _this.parents(opt.errElem),
+					_url = _this.data('url'),
+					_group = _this.data('group'),
+					_name = _this.attr('name'),
+					_re = reTxt[ _name ];
+
+				//errElem
+				_parent = opt.errElem ? _this.parents(opt.errElem) : _parent;
+
+				if ( v_empty(_this) ){
+					_re && v_pattern(_this);
+					_group && v_group(_this);
+					if ( eventName == 'blur' ) {
+						_url && v_url(_this);
+					}
+				}
+			}
 
 			oRequired.live('blur', function(){
-				var _this = $(this),
-					_parent = _this.parent(),
-					_name = _this.attr('name'),
-					_re = reTxt[ _name ],
-					_val = $.trim( _this.val() );
-
-				//errElem
-				_parent = opt.errElem ? _this.parents(opt.errElem) : _parent;
-
-				function v_empty(){
-					_parent[ (!_val ? 'add' : 'remove') + 'Class' ](opt.errClass);
-					_parent[ (!_val ? 'remove' : 'add') + 'Class' ](opt.okClass);
-					return !_val ? false : true;
-				}
-
-				function v_pattern(){
-					
-					var flag = false;
-					var reg = new RegExp(_re);
-					_parent[ (!reg.test(_val) ? 'add' : 'remove') + 'Class' ](opt.errClass);
-					_parent[ (!reg.test(_val) ? 'remove' : 'add') + 'Class' ](opt.okClass);
-					if( !reg.test(_val) ){
-						flag = false;
-					}else{
-						flag = true;
-						opt.regexped( _this );
-					}
-					return flag;
-					
-				}
-
-				if ( v_empty() && v_pattern() ) { setSaveBtn(); }
+				var _this = $(this);
+				v_event(_this, 'blur');
+				setSaveBtn();
 			}).live('keyup', function(){
-				var _this = $(this),
-					_datas = _this.data(),
-					_name = _this.attr('name'),
-					_parent = _this.parent(),
-					_re = reTxt[ _name ],
-					_group = _this.data('group'),
-					_url = _this.data('url'),
-					_val = $.trim( _this.val() );
-
-				//errElem
-				_parent = opt.errElem ? _this.parents(opt.errElem) : _parent;
-
-				function v_empty(){
-					_parent[ (!_val ? 'add' : 'remove') + 'Class' ](opt.errClass);
-					_parent[ (!_val ? 'remove' : 'add') + 'Class' ](opt.okClass);
-					return !_val ? false : true;
-				}
-
-				function v_pattern(){
-					
-					var flag = false;
-					var reg = new RegExp(_re);
-					_parent[ (!reg.test(_val) ? 'add' : 'remove') + 'Class' ](opt.errClass);
-					_parent[ (!reg.test(_val) ? 'remove' : 'add') + 'Class' ](opt.okClass);
-					if( !reg.test(_val) ){
-						flag = false;
-					}else{
-						flag = true;
-						opt.regexped( _this );
-					}
-					return flag;
-					
-				}
-
-				function v_url(){
-					var data = {};
-					var flag = false;
-					data[_name] = _val;
-					$.ajax({
-						async: false,
-						type: 'post',
-						dataType: 'json',
-						data: data,
-						url:_url,
-						success:function(res){
-							_parent[ (!res.code ? 'add' : 'remove') + 'Class' ](opt.errClass);
-							_parent[ (!reg.code ? 'remove' : 'add') + 'Class' ](opt.okClass);
-							if( res.code == 1 ){
-								flag = true;
-								_parent.removeClass(opt.errClass);
-							}else{
-								flag = false;
-								_parent.addClass(opt.errClass);
-							}
-							opt.urled(_this, res);
-						}
-					});
-
-					return flag;
-					
-				}
-
-				function v_group(){
-
-					var oGroup = oForm.find('input[data-group="'+_group+'"]');
-					var arr = [];
-					var flag = false;
-					oGroup.each(function(i, ele){
-						if( !$.trim($(ele).val()) ){ arr.push(i); }
-					});
-
-					if( !arr.length ){
-						if( oGroup.eq(0).val() !== oGroup.eq(1).val() ){
-							flag = false;
-							_parent.addClass(opt.errClass);
-							_parent.removeClass(opt.okClass);
-						}else{
-							flag = true;
-							_parent.removeClass(opt.errClass);
-							_parent.addClass(opt.okClass);
-						}
-					}
-
-					return flag;
-
-				}
-
-				//is empty
-				if ( v_empty() ){
-					//pattern
-					if ( _re && v_pattern() ) {
-						//ajax
-						if (_url) {v_url(); }
-						//group
-						if (_group) {v_group();}
-					}
-					if ( !_re ) {
-						//ajax
-						if (_url) {v_url(); }
-						//group
-						if (_group) {v_group();}
-					}
-				}
-
+				var _this = $(this);
+				v_event(_this, 'keyup');
 				setSaveBtn();
 			});
 
